c5281976d4bca14d1ef555aaacca78343eb65214
Raghav Goel <67116823+raghavgoel24@users.noreply.github.com>
2025-03-23 19:28:24 +0530

    Update streamlit_app.py
    Added sample code for SmoothieFruit API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -42,3 +43,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
